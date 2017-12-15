d05eb6b3f6634e049893f50c97e48ad6745b8098
kobarjan <kobarjan@onet.pl>
2017-12-15 21:11:56 +0100

    not working radio

--- radio/src/sample/MetadataGetter.java
+++ radio/src/sample/MetadataGetter.java
@@ -1,4 +1,29 @@
 package sample;
 
+import net.moraleboost.streamscraper.ScrapeException;
+import net.moraleboost.streamscraper.Scraper;
+import net.moraleboost.streamscraper.Stream;
+import net.moraleboost.streamscraper.scraper.IceCastScraper;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
 public class MetadataGetter {
+    Scraper scraper = new IceCastScraper();
+    List<Stream> streams = scraper.scrape(new URI("http://wnuq.pl:8000/radio.44100"));
+
+    public MetadataGetter() throws URISyntaxException, ScrapeException {
+    }
+
+    public String CurrentSong() {
+        String response = null;
+        for (Stream stream : streams){
+            response = stream.getCurrentSong();
+        }
+        return response;
+    }
 }
--- radio/src/sample/Radio.java
+++ radio/src/sample/Radio.java
@@ -1,7 +1,17 @@
 package sample;
 
-import javax.sound.sampled.*;
+import net.moraleboost.streamscraper.ScrapeException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioFormat.Encoding;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import static sample.RadioController.getGain;
@@ -13,6 +23,7 @@ public class Radio implements Runnable{
     private String name = "radio";
     public boolean shutdown = false;
     SourceDataLine line;
+    //MetadataGetter metadataGetter;
     public void play(){
         try {
             AudioInputStream in = AudioSystem.getAudioInputStream(new URL("http://wnuq.pl:8000/radio.44100"));
@@ -23,11 +34,13 @@ public class Radio implements Runnable{
             byte[] buffer = new byte[4096];
             DataLine.Info info = new DataLine.Info(SourceDataLine.class,targetFormat);
             line = (SourceDataLine) AudioSystem.getLine(info);
-
+            //metadataGetter = new MetadataGetter();
+            //System.out.println(metadataGetter.CurrentSong());
             if(line != null){
                 line.open();
                 line.start();
                 setGain(getGain());
+                //CurrentSong();
                 int nBytesRead = 0;
                 while (nBytesRead != -1 && shutdown != true) {
                     nBytesRead = dataIn.read(buffer, 0, buffer.length);
@@ -55,7 +68,11 @@ public class Radio implements Runnable{
             e.printStackTrace();
         } catch (LineUnavailableException e) {
             e.printStackTrace();
-        }
+        } /*catch (ScrapeException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }*/
     }
     public void start(){
         if (t == null) {
